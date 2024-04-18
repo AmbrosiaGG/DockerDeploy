b6bd059a75d62a1e519db84bfd5dd798e18445df
duckydev <duckydev@ambrosia.gg>
2024-04-18 11:32:34 +0630

    Initial commit for server

---
+++ index.js
@@ -0,0 +1,30 @@
+const Docker = require('dockerode');
+const express = require('express')
+
+const app = express()
+
+let bodyParser = require('body-parser')
+app.use(require('cors')())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+const docker = new Docker(); // Default docker sock
+
+console.log('Re-Building base image :D please wait')
+require("child_process").execSync('cd ./docker/cloud_deploy/base && docker build -t cloudbase .')
+
+docker.createContainer({
+    Image: 'cloudbase',
+    AttachStdin: true,
+    AttachStdout: true,
+    AttachStderr: true,
+    Tty: true,
+    OpenStdin: true,
+    StdinOnce: false
+  })
+
+app.listen(3094, async () => {
+    console.log('Whats the password? 3094? correct')
+})
+
